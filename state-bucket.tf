b38366401524d50eb31dac0126b9eb4304b4b0df
Sarros <sarros@Tank.local>
2019-03-24 17:26:58 +1100

    Terraform re-init for backend update

--- state-bucket.tf
+++ state-bucket.tf
@@ -1,3 +1,6 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Setup s3 bucket with versioning for state file storage
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 resource "aws_s3_bucket" "terraform_state_repo" {
   bucket = "terraform-state-repo"
 
@@ -5,4 +8,24 @@ resource "aws_s3_bucket" "terraform_state_repo" {
     enabled = true
   }
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags {
+    Name = "Remote s3 storage to manange Terraform state files"
+  }
+
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Create Terraform backend resource for state management
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "terraform-state-repo"
+    region = "ap-southeast-2"
+    key = "terraform.tfstate"
+  }
 }
